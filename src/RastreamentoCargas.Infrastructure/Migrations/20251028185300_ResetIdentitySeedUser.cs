87f586c2ce95c6fae55547244eccfdd4b42105b6
Paulo <pmatos2000@gmail.com>
2025-10-28 15:53:41 -0300

    ResetIdentitySeedUser

---
+++ src/RastreamentoCargas.Infrastructure/Migrations/20251028185300_ResetIdentitySeedUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RastreamentoCargas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ResetIdentitySeedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DBCC CHECKIDENT('AspNetUsers', RESEED, 0);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
