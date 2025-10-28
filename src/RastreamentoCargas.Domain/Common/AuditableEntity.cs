97e1f018d3e8009bc07c165eb2196801c90045f2
Paulo <pmatos2000@gmail.com>
2025-10-27 21:01:45 -0300

    Corrige sintaxe em AuditableEntity.cs
    Remove ponto e vírgula desnecessário após a declaração da propriedade `CreatedBy` no arquivo `AuditableEntity.cs`. Isso corrige a sintaxe da declaração da propriedade, garantindo conformidade com as práticas de codificação.

--- src/RastreamentoCargas.Domain/Common/AuditableEntity.cs
+++ src/RastreamentoCargas.Domain/Common/AuditableEntity.cs
@@ -5,7 +5,7 @@ namespace RastreamentoCargas.Domain.Common
     public abstract class AuditableEntity : BaseEntity, IAuditable
     {
         public DateTime CreatedAt { get; set; }
-        public required string CreatedBy { get; set; };
+        public required string CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
         public string? UpdatedBy { get; set; }
     }
