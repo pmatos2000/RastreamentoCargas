699395a5d9da39a7be97446da13bca084a0fe9df
Paulo <pmatos2000@gmail.com>
2025-10-28 02:56:36 -0300

    Remover usuário inicial do banco de dados
    As alterações removem o usuário inicial com ID "a18be9c0-aa65-4af8-bd17-00bd9344e575" da tabela "AspNetUsers".
    A migração `DelUserRoot` foi criada para excluir este usuário no método `Up` e reinseri-lo no método `Down`.
    O arquivo `AppDbContextModelSnapshot.cs` foi atualizado para refletir essa remoção, e um arquivo de designer para a migração foi gerado.

--- src/RastreamentoCargas.Infrastructure/Migrations/20251028055553_DelUserRoot.Designer.cs
+++ src/RastreamentoCargas.Infrastructure/Migrations/20251028055553_DelUserRoot.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RastreamentoCargas.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using RastreamentoCargas.Infrastructure.Data;
 namespace RastreamentoCargas.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251028055553_DelUserRoot")]
+    partial class DelUserRoot
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/RastreamentoCargas.Infrastructure/Migrations/20251028055553_DelUserRoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RastreamentoCargas.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DelUserRoot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "CreatedAt", "CreatedBy", "Discriminator", "Email", "EmailConfirmed", "FullName", "IsActive", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UpdatedAt", "UpdatedBy", "UserName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, "5cf97b44-6355-46ca-8b09-854365615a72", new DateTime(2025, 10, 28, 5, 22, 20, 785, DateTimeKind.Utc).AddTicks(1367), "System", "User", "root@sistema.com", true, "Administrador Raiz", true, false, null, "ROOT@SISTEMA.COM", "ROOT", "AQAAAAIAAYagAAAAEI7DEXLRpqJNdXsklN2jlaRgesTkHDUFV+LErvU0PMgeN6SLPtBYQ1M+3beDaJwshw==", null, false, "16df264e-c9c3-4b27-ad9f-f45da9cdfd60", false, null, null, "root" });
+        }
+    }
+}
